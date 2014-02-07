6117d1a55fc32836ec2860e0fb62092a24583524
Evgenia Lyjina <jenny.lyjina@gmail.com>
2014-02-07 17:48:08 +0200

    testeja

--- Laivanupotus/src/test/java/laivanupotus/logiikka/PlayerTest.java
+++ Laivanupotus/src/test/java/laivanupotus/logiikka/PlayerTest.java
@@ -6,94 +6,115 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
 
-    Player pelaaja;
+    Player red;
+    Player black;
 
     public PlayerTest() {
     }
 
     @Before
     public void setUp() {
-        pelaaja = new Player("testaaja", 10);
+        red = new Player("testaaja", 10);
+        black = new Player("ammuttava", 10);
     }
 
     @Test
     public void konstruktoriLuoPelaajanTest() {
-        assertEquals("testaaja", pelaaja.getName());
+        assertEquals("testaaja", red.getName());
     }
 
     @Test
     public void arvoLaivaPysyyAlueellaTest() {
-        Ship uusi = pelaaja.arvoLaiva(3);
-        if(uusi.isOrientation()){
-        int xend = uusi.getSize() - 1 + uusi.getX();
-        assertTrue((uusi.getX() >= 0) && (xend < 10) && (uusi.getY() >= 0) && (uusi.getY() < 10));
-        }else{
-        int yend = uusi.getSize() - 1 + uusi.getY();
-        assertTrue((uusi.getX() >= 0) && (uusi.getX() < 10) && (uusi.getY() >= 0) && (yend < 10));
+        Ship uusi = red.arvoLaiva(3);
+        if (uusi.isOrientation()) {
+            int xend = uusi.getSize() - 1 + uusi.getX();
+            assertTrue((uusi.getX() >= 0) && (xend < 10) && (uusi.getY() >= 0) && (uusi.getY() < 10));
+        } else {
+            int yend = uusi.getSize() - 1 + uusi.getY();
+            assertTrue((uusi.getX() >= 0) && (uusi.getX() < 10) && (uusi.getY() >= 0) && (yend < 10));
         }
     }
 
     @Test
     public void arvoLaivaEiLisaaLaivastoaNegatiivisillaTest() {
-        Ship uusi = pelaaja.arvoLaiva(-1);
-        int laivastonKoko = pelaaja.getFleat().size();
+        Ship uusi = red.arvoLaiva(-1);
+        int laivastonKoko = red.getFleat().size();
         assertEquals(0, laivastonKoko);
     }
 
     @Test
     public void arvoLaivaEiLisaaLaivastoaYliAlueenArvoillaTest() {
-        Ship uusi = pelaaja.arvoLaiva(11);
-        int laivastonKoko = pelaaja.getFleat().size();
+        Ship uusi = red.arvoLaiva(11);
+        int laivastonKoko = red.getFleat().size();
         assertEquals(0, laivastonKoko);
     }
-    
+
     @Test
     public void arpoLaivaKoollaOnValiaTest() {
-        assertNull(pelaaja.arvoLaiva(-3));
+        assertNull(red.arvoLaiva(-3));
     }
 
     @Test
     public void asetaLaivaKoollaOnValiaTest() {
-        assertNull(pelaaja.arvoLaiva(-3));
+        assertNull(red.arvoLaiva(-3));
     }
 
     @Test
     public void asetaLaivaPalauttaaNullVaarillaArvoillaTest() {
-        assertEquals(null, pelaaja.asetaLaiva(10, 10, 1, true));
+        assertEquals(null, red.asetaLaiva(10, 10, 1, true));
     }
 
     @Test
     public void asetaLaivaPalauttaaNullVaarillaArvoilla2Test() {
-        assertEquals(null, pelaaja.asetaLaiva(-1, 1, 1, true));
+        assertEquals(null, red.asetaLaiva(-1, 1, 1, true));
     }
 
     @Test
     public void asetaLaivaPalauttaaLaivanOikeillaArvoillaTest() {
-        String res = pelaaja.asetaLaiva(3, 3, 3, true).toString();
+        String res = red.asetaLaiva(3, 3, 3, true).toString();
         assertEquals("x = 3, y = 3, koko = 3, suunta = vertical, health = 3", res);
     }
 
     @Test
     public void asetaLaivaPalauttaaLaivanOikeillaArvoilla2Test() {
-        String res = pelaaja.asetaLaiva(3, 3, 3, false).toString();
+        String res = red.asetaLaiva(3, 3, 3, false).toString();
         assertEquals("x = 3, y = 3, koko = 3, suunta = horisontal, health = 3", res);
     }
 
     @Test
     public void asetaLaivaLisaaLaivastoonOikeinTest() {
-        pelaaja.asetaLaiva(0, 0, 3, true).toString();
-//        pelaaja.asetaLaiva(0, 0, 3, false).toString();
-//        pelaaja.asetaLaiva(0, 3, 3, true).toString();
-        pelaaja.asetaLaiva(2, 2, 3, false).toString();
+        red.asetaLaiva(0, 0, 3, true).toString();
+//        red.asetaLaiva(0, 0, 3, false).toString();
+//        red.asetaLaiva(0, 3, 3, true).toString();
+        red.asetaLaiva(2, 2, 3, false).toString();
 
-        assertSame(2, pelaaja.getFleat().size());
+        assertSame(2, red.getFleat().size());
     }
 
     @Test
     public void arvoLaivaLisaaLaivastoonOikeinTest() {
-        pelaaja.arvoLaiva(4);
-        pelaaja.arvoLaiva(3);
-        pelaaja.arvoLaiva(-3);
-        assertSame(2, pelaaja.getFleat().size());
+        red.arvoLaiva(4);
+        red.arvoLaiva(3);
+        red.arvoLaiva(-3);
+        assertSame(2, red.getFleat().size());
+    }
+
+    @Test
+    public void shootOsuessaanPalauttaaTrueTest() {
+        black.asetaLaiva(0, 0, 1, true);
+        assertTrue(red.shoot(black, 0, 0));
+    }
+
+    @Test
+    public void shootEiOsuessaanPalauttaaFalseTest() {
+        black.asetaLaiva(0, 0, 1, true);
+        assertFalse(red.shoot(black, 0, 1));
+    }
+
+    @Test
+    public void shootTappaessaanPoistaaLaivanLaivastostaTest() {
+//        black.asetaLaiva(0, 0, 1, true);
+        red.shoot(black, 0, 0);
+        assertEquals(0, black.getFleat().size());
     }
 }
