92e48edf905fc6fb1722f0acfc2c0adf117e78a6
Evgenia Lyjina <jenny.lyjina@gmail.com>
2014-02-25 13:45:29 +0200

    tuntikirjanpito ja testikansion filet

---
+++ Laivanupotus/src/main/java/laivanupotus/listeners/HideShipsListener.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package laivanupotus.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author evly
+ */
+public class HideShipsListener implements ActionListener {
+
+    public HideShipsListener() {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
